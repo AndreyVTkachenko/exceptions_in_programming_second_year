30e3133163eb337092e7b48571ae02f2b05ca7ed
«Andrew» <gepatytich@gmail.com>
2024-11-04 23:19:06 +0700

    added homework

---
+++ src/main/java/gb/homework/Task02_source.java
@@ -0,0 +1,12 @@
+package gb.homework;
+
+public class Task02_source {
+
+//    try {
+//        int d = 0;
+//        double catchedRes1 = intArray[8] / d;
+//        System.out.println("catchedRes1 = " + catchedRes1);
+//    } catch (ArithmeticException e) {
+//        System.out.println("Catching exception: " + e);
+//    }
+}

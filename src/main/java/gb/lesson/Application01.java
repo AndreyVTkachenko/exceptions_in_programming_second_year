cca1b1b958b458b1971e636676c24e29893a535c
«Andrew» <gepatytich@gmail.com>
2024-11-17 10:47:53 +0700

    added lesson

--- src/main/java/gb/lesson/Application01.java
+++ src/main/java/gb/lesson/Application01.java
@@ -1,8 +1,23 @@
 package gb.lesson;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Application01 {
     public static void main(String[] args) {
-        String name = null;
-        System.out.println(name.length());
+        try (FileReader reader = new FileReader("C:\\Users\\gepat\\Documents\\training at GeekBrains" +
+                                                "\\Исключения в программировании и их обработка\\exceptions_in_programming_second_year" +
+                                                "\\src\\main\\java\\gb\\lesson\\test.txt");
+             FileWriter writer = new FileWriter("C:\\Users\\gepat\\Documents\\training at GeekBrains" +
+                                                "\\Исключения в программировании и их обработка\\exceptions_in_programming_second_year" +
+                                                "\\src\\main\\java\\gb\\lesson\\file.txt")) {
+            while (reader.ready()) {
+                writer.write(reader.read());
+            }
+        } catch (RuntimeException | IOException e) {
+            System.out.println("Поймано исключение: " + e.getClass().getSimpleName());
+        }
+        System.out.println("Копирование завершено.");
     }
 }
--- src/main/java/gb/lesson/Application03.java
+++
@@ -1,9 +0,0 @@
-package gb.lesson;
-
-public class Application03 {
-    public static void main(String[] args) {
-        String number = "123fq";
-        int result = Integer.parseInt(number);
-        System.out.println(result);
-    }
-}
